e7dec1ca51eacbd72ee20ea0467b080a3956d243
SanclassDEV <151254996+Sancley@users.noreply.github.com>
2024-11-19 13:55:15 +0000

    alterações e bug fix

--- prcalendario/src/App.js
+++ prcalendario/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Calendario from './page/Calendario'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+import './page/components/Components-Calendario-css.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Calendario/>
     </div>
   );
 }
